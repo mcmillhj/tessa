6fa4c6bfb7ca293611a19f42f10247887bc43d1c
Hunter McMillen <mcmillhj@gmail.com>
2016-01-27 17:09:12 -0500

    return asset_id when retrieving note records

--- lib/tessa/db/mysql.pm
+++ lib/tessa/db/mysql.pm
@@ -25,7 +25,7 @@ has dbh => (
 );
 
 sub _build_dsn {
-    return 'DBI:mysql:;port=3306;database=tessa;host=192.34.60.158';
+    return 'DBI:mysql:;port=3306;database=tessa;host=192.34.60.159';
 }
 
 sub _build_dbh {
@@ -139,7 +139,8 @@ sub get_all_notes_for_asset {
 
     my $note_sth = $self->dbh->prepare(q{
        select id, 
-              note 
+              note,
+	      asset_id
        from tessa.notes
        where asset_id = ?;
     });
